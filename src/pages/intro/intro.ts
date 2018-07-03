f847025c36016ca4e8fdec3f6a0d83f173f51ff7
Adegoke Saidi <rasheedsaidi@gmail.com>
2018-07-03 06:50:53 +0100

    Get Intro page content in place

---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the IntroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
+})
+export class IntroPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+ 
+  goToHome(){
+    this.navCtrl.setRoot(HomePage);
+  }
+
+}
